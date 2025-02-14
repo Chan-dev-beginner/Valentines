46d55d7166360dd15b0255ee0a00eeb287a262db
Chan-dev-beginner <cbejerano13@gmail.com>
2025-02-14 11:45:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY VALENTINE‘S NORII<3').split('')
+    const titles = ('HAPPY VALENTINE‘S NIFNIF<3').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
